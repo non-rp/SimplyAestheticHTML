25aff27333346b3bb73ae9ff72dd154ab7f56bd7
Valentin <non.rp.1996@gmail.com>
2022-05-24 20:42:02 +0300

    animation and hovers

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,7 +4,7 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 
-
+// Mega menu
     document.addEventListener('click' , documentActions);
 
     function documentActions(e) {
@@ -24,13 +24,59 @@ import { flsModules } from "./modules.js";
         }
         
     }
+// Megamenu End
 
+
+//Masonry reviews block
 
 var elem = document.querySelector('.reviews__wrap');
 var msnry = new Masonry( elem, {
-  // options
   itemSelector: '.reviews__item',
   columnWidth: '.reviews__item',
   percentPosition: true,
   gutter: 30
 });
+
+//End masonry reviews block
+
+// Animation 
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			} else {
+				if (!animItem.classList.contains('_anim-no-hide')) {
+					animItem.classList.remove('_active');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+
+	setTimeout(() => {
+		animOnScroll();
+	}, 300);
+}
+
+
+// End Animation
